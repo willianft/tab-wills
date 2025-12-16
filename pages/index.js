1c6374c83056b2a28cf543bf7f1794ce5ce9b3ba
willianft <willianft10@outlook.com>
2025-12-16 17:23:43 +0000

    atualizado a frase inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Se você me ama, dá uma risadinha </h1>
+    return <h1>Nos reencontramos mais maduros, com novas perspectivas e o coração aberto para recomeçar. O tempo trouxe aprendizados, mas o sentimento permaneceu, e hoje sigo feliz por ter a oportunidade de construir algo novo ao seu lado, com carinho, respeito e amor. Te Amo! ❤️ </h1>
 }
 
 export default Home;
