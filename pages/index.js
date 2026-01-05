2185055bf51288f2b41776de2a53a0a168f9a7a2
willianft <willianft10@outlook.com>
2026-01-05 19:40:08 +0000

    adiciona scripts `lint:check` e `lint:fix`
    q

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 function Home() {
-    return <h1>Nos reencontramos mais maduros, com novas perspectivas e o coração aberto para recomeçar. O tempo trouxe aprendizados, mas o sentimento permaneceu, e hoje sigo feliz por ter a oportunidade de construir algo novo ao seu lado, com carinho, respeito e amor. Te Amo! ❤️ </h1>
-}
+  return (
+    <h1>
+      Nos reencontramos mais maduros, com novas perspectivas e o coração aberto
+      para recomeçar. O tempo trouxe aprendizados, mas o sentimento permaneceu,
+      e hoje sigo feliz por ter a oportunidade de construir algo novo ao seu
+      lado, com carinho, respeito e amor. Te Amo! ❤️{" "}
+    </h1>
+  );
 
+  function teste() {
+    console.log("Teste");
+  }
+}
 export default Home;
