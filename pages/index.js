7eec99dcbf422459b0870a55fe29eee610fcfc7b
willianft <willianft10@outlook.com>
2026-01-09 16:22:30 +0000

    `alteração pagina em construção`

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,4 @@
 function Home() {
-  return (
-    <h1>
-      Nos reencontramos mais maduros, com novas perspectivas e o coração aberto
-      para recomeçar. O tempo trouxe aprendizados, mas o sentimento permaneceu,
-      e hoje sigo feliz por ter a oportunidade de construir algo novo ao seu
-      lado, com carinho, respeito e amor. Te Amo! ❤️{" "}
-    </h1>
-  );
+  return <h1> Em Contrução ... </h1>;
 }
 export default Home;
